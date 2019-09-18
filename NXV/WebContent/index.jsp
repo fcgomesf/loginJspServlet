71ff9496724517e6546d8bfb6210934cc87b83d3
200004685 <200004685@PT1L303.ucsal.br>
2019-09-18 09:20:18 -0300

    outra att q nseidinada

--- NXV/WebContent/index.jsp
+++ NXV/WebContent/index.jsp
@@ -5,13 +5,15 @@
 <title>Insert title here</title>
 </head>
 <body>
-<!-- <form action="verificador.jsp" method="post">  -->
-<form action="ServDeixa" method="post">
-<input type="text" name="login" placeholder="nome de usuario">
-<input type="password" name="senha" placeholder="passuorde">
-<br>
-<button type="submit">Confirmar</button>
-</form>
+	<form action="ServDeixa" method="post">
+
+		Usuário: <input type="text" name="login" placeholder="nome de usuario"><br>
+		Senha: <input type="password" name="senha" placeholder="passuorde"><br>
+		<button type="submit">Confirmar</button>
+		<% if (request.getAttribute("erro") != null) {	%>
+		<% request.getAttribute("erro"); %>
+		<% } %>
+	</form>
 </body>
 </html>
 
--- NXV/src/br/ucsal/ServDeixa.java
+++ NXV/src/br/ucsal/ServDeixa.java
@@ -11,33 +11,39 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/ServDeixa")
 public class ServDeixa extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    public ServDeixa() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
-		
-		if(login.equals(senha)){
-			
-			RequestDispatcher dispatch = request.getRequestDispatcher("verificador.jsp");
-			dispatch.forward(request, response);
-			
-		}else{
-			response.sendRedirect("index.html?erro=Acesso Negado");
-			
-			 }
-				
-		response.getWriter().append("<html> <body>");
 
-		response.getWriter().append("</body></html>");
-		
-		
+		Usuario user = new Usuario();
+		user.setLogin(login);
+		user.setSenha(senha);
+
+		if (this.autenticado(user)) {
+			request.setAttribute("usuario", user);
+			RequestDispatcher dispatch = request.getRequestDispatcher("dashboard.jsp");
+			dispatch.forward(request, response);
+
+		} else {
+			request.setAttribute("erro", "Acesso Negado");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+			dispatcher.forward(request, response);
+
+		}
+	}
+
+	private boolean autenticado(Usuario usuario) {
+		boolean resultado = false;
+		if (usuario != null && usuario.getLogin() != null && usuario.getSenha() != null
+				&& usuario.getLogin().equals(usuario.getSenha())) {
+			resultado = true;
+		}
+		return resultado;
 	}
 
 }
